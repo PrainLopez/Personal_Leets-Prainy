219008e77eb9f44139a1e8b78ae00bb9cee85f1a
PrainLopez <qym200042@gmail.com>
2024-06-25 12:21:32 +0100

    20240625 538/1038 TypeScript

---
+++ TypeScript/538(1038)_Convert_BST_to_Greater_Tree/solution.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function convertBST(root: TreeNode | null): TreeNode | null {
+    const stack: TreeNode[] = [];
+    let currNode: TreeNode | null = root;
+    let currSum = 0;
+
+    while (stack.length || currNode) {
+        while (currNode) {
+            stack.push(currNode);
+            currNode = currNode.right;
+        }
+
+        currNode = stack.pop() as TreeNode;
+        currSum += currNode?.val;
+        currNode.val = currSum;
+
+        currNode = currNode.left;
+    }
+
+    return root;
+}
