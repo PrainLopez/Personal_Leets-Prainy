06ce1383b395be98ab8ac7c48a48179a3ff31df2
PrainLopez <qym200042@gmail.com>
2024-11-10 23:57:43 +0000

    20241110 122 Java

---
+++ Java/122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            final int diff = prices[i + 1] - prices[i];
+
+            if (diff > 0) {
+                profit += diff;
+            }
+        }
+
+        return profit;
+    }
+}
