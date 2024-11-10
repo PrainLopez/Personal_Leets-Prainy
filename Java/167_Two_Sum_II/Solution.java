cea3e60add39501839371c15a85df45ace6993b5
PrainLopez <qym200042@gmail.com>
2024-11-10 21:15:08 +0000

    20241110 167 Java

---
+++ Java/167_Two_Sum_II/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int leftPtr = 0;
+        int rightPtr = numbers.length - 1;
+        int[] res = new int[2];
+
+        while (leftPtr < rightPtr) {
+            final int sum = numbers[leftPtr] + numbers[rightPtr];
+            if (sum == target) {
+                res[0] = leftPtr + 1;
+                res[1] = rightPtr + 1;
+                return res;
+            } else if (sum > target) {
+                rightPtr--;
+            } else {
+                leftPtr++;
+            }
+        }
+        return res;
+    }
+}~
