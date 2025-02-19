70cf169761316f066e6635477f5bd6da825f49a3
PrainLopez <qym200042@gmail.com>
2025-02-19 13:08:38 +0000

    20250219 21 Java

---
+++ Java/LC21_Merge_Two_Sorted_Lists/ListNode.java
@@ -0,0 +1,18 @@
+package LC21_Merge_Two_Sorted_Lists;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
