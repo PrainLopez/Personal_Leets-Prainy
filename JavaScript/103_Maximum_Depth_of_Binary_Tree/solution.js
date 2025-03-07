02c5716f5b679cbb75c4a6d80899cad2416adc60
Prainy <qym200042@gmail.com>
2025-03-07 14:15:18 +0000

    20250307 103 JavaScript

---
+++ JavaScript/103_Maximum_Depth_of_Binary_Tree/solution.js
@@ -0,0 +1,17 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+    if (root === null) {
+        return 0
+    }
+
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
