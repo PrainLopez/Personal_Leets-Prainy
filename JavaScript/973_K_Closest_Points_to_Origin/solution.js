13df4dbf2399fc24904741299abaa9cff9296a64
Prainy <qym200042@gmail.com>
2025-09-22 15:40:14 +0100

    20250922 973 JavaScript

---
+++ JavaScript/973_K_Closest_Points_to_Origin/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+const kClosest = function (points, k) {
+    return points.sort((a, b) => (a[0] ** 2 + a[1] ** 2) - (b[0] ** 2 + b[1] ** 2)).slice(0, k);
+};
